8edb6ba009038d85cecb67077c3d140d1d17e6be
Paul Kaifler <100681222+PaulKaifler@users.noreply.github.com>
2024-05-08 23:41:27 +0200

    Update web.sh

--- web.sh
+++ web.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
 # check if current directory is a git repository
-if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
-
-    # check if there is a remote origin configured for the current repository
-    if !(git remote >/dev/null 2>&1); then
-        echo "No remote origin found" >&2
-        exit 1
-    fi
-
-    REPO_URL=$(git config --get remote.origin.url)
-    REPO_URL=${REPO_URL//:/\/}  # replace : with /\ in the URL
-    REPO_URL=${REPO_URL/#git@/https:\/\/}  # add https:\/\/ for git@ URLs
-
-    open -a Safari $REPO_URL
-
-    unset REPO_URL
-
-else
-    echo "Current dir is not a git repository" >&2
-    exit 1
+if ! git rev-parse --is-inside-work-tree &> /dev/null; then
+    echo "Not a git repository. Exiting."
+    return 1
 fi
+
+# check if there is a remote origin configured for the current repository
+if ! git remote -v | grep -qE '^\S+\s+\S+'; then
+    echo "No remote set up for the git repository. Exiting."
+    return 1
+fi
+
+REPO_URL=$(git config --get remote.origin.url)
+REPO_URL=${REPO_URL//:/\/}  # replace : with /\ in the URL
+REPO_URL=${REPO_URL/#git@/https:\/\/}  # add https:\/\/ for git@ URLs
+
+open -a Safari $REPO_URL
+
+unset REPO_URL
